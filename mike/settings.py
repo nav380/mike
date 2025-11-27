01fa74a9bd0d057e49b04c08f2353c3eed815564
nav380 <naveenchauhan380@gmail.com>
2025-11-27 17:25:08 +0530

    commit

--- mike/settings.py
+++ mike/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "naveenchauhan380@gmail.com"
-EMAIL_HOST_PASSWORD = "qems sdas ljbk pjsr"
+EMAIL_HOST_USER = "info@vaicon2026.com"
+EMAIL_HOST_PASSWORD = "bxls wgtw ympu bcki"
 
 AUTH_USER_MODEL = "registration.registeredUser"
 
